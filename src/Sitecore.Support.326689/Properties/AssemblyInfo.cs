e7ca10d7618ac5c90ba45c69261895d6a84d35d4
DK\whc <whc@sitecore.net>
2019-04-25 20:41:26 +0800

    Fix SXA composite item datasource rendering variant not saved for custom culture

---
+++ src/Sitecore.Support.326689/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.326689")]
+[assembly: AssemblyProduct("Sitecore.Support.326689")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.326689/SaveCompositeItems.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Sitecore.Support.XA.Feature.Composites.EventHandlers
+{
+  // SaveCompositeItems
+  using Sitecore.Data.Items;
+  using Sitecore.Pipelines;
+  using Sitecore.Pipelines.ResolveRenderingDatasource;
+
+  public class SaveCompositeItems : Sitecore.XA.Feature.Composites.EventHandlers.SaveCompositeItems
+  {
+    protected override Item GetCompositeDatasource(Item contextItem, string datasource)
+    {
+      ResolveRenderingDatasourceArgs resolveRenderingDatasourceArgs = new ResolveRenderingDatasourceArgs(datasource);
+      resolveRenderingDatasourceArgs.CustomData["contextItem"] = contextItem;
+      CorePipeline.Run("resolveRenderingDatasource", resolveRenderingDatasourceArgs, false);
+      return contextItem.Database.GetItem(resolveRenderingDatasourceArgs.Datasource, contextItem.Language);
+    }
+  }
+}
